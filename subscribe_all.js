d32deb55d70df6d99c30e399f479ef4464b46a4a
Mahmoud Gamal <smartmoody3@gmail.com>
2017-01-15 18:30:55 +0200

    Subscribe_all,js

---
+++ subscribe_all.js
@@ -0,0 +1,25 @@
+function sendXHR(reqType,address, asyncProc){
+   var r = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+   r.open(reqType, address, false);
+   (reqType == "PATCH" ? r.setRequestHeader("X-CSRF-Token", current_user.get("csrf_token")) : 1)
+   r.send();
+   return r;
+  }
+  var programs = new Array();
+  var pros = JSON.parse(sendXHR("GET","/programs/search.json?query=type%3Ahackerone&sort=published_at%3Adescending&page=1").responseText);
+  for(i=0; i<pros.results.length; i++){
+  programs.push(pros.results[i].url);
+  }
+  for(i=0;i<programs.length;i++){
+	  try {
+	sendSub = sendXHR("PATCH",programs[i]+"/policy_subscriptions");
+	if(sendSub.status != 200) {
+		for(x=429;x != 200 ;x=sendXHR("PATCH",programs[i]+"/policy_subscriptions").status){} // Keep repeating the request untill we are out of rate limit :D
+	}
+   sub = JSON.parse(sendSub.responseText);
+   (sub.subscription == false) ? sendXHR("PATCH",programs[i]+"/policy_subscriptions"): 1;
+	  } 
+	  catch (e){
+		  console.log(e+" don't worry we are still in the loop");
+	  }
+  }
